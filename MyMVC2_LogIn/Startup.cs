f5364e1edbff1240e6b2138787fb376843dc7b87
StudeyPC <zhzh@tongji.edu.cn>
2019-05-18 20:55:20 +0800

    Init Template

---
+++ MyMVC2_LogIn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMVC2_LogIn.Startup))]
+namespace MyMVC2_LogIn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
